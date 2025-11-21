17424aca46353e138a9440b2776cd4c8c8a7ac6c
raymond yong <ryew.1984@gmail.com>
2025-11-21 22:22:23 +0100

    feat: add post-processing for hover outlines on furniture items and encapsulate functionality into a new Furniture component

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,7 +1,26 @@
-import React, { useRef } from 'react';
+
+import React, { useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Box, Plane, Sphere, Cylinder, SoftShadows } from '@react-three/drei';
+import { Selection, Select, EffectComposer, Outline } from '@react-three/postprocessing';
 import { Draggable } from './Draggable';
+
+function Furniture({ children, initialPosition }) {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <Draggable initialPosition={initialPosition}>
+      <Select enabled={hovered}>
+        <group
+          onPointerOver={(e) => { e.stopPropagation(); setHovered(true); }}
+          onPointerOut={(e) => { e.stopPropagation(); setHovered(false); }}
+        >
+          {children}
+        </group>
+      </Select>
+    </Draggable>
+  );
+}
 
 export function Scene({ brightness }) {
   // Calculate light intensity based on brightness prop (0 to 1)
@@ -52,98 +71,105 @@ export function Scene({ brightness }) {
         castShadow
       />
 
-      {/* Room Structure */}
-      <group position={[0, -1.5, 0]}>
-        {/* Floor */}
-        <Plane
-          args={[10, 10]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          receiveShadow
-        >
-          <meshStandardMaterial color="#333" roughness={0.8} />
-        </Plane>
+      <Selection>
+        <EffectComposer autoClear={false} multisampling={8}>
+          <Outline blur edgeStrength={10} width={1000} visibleEdgeColor="white" hiddenEdgeColor="white" />
+        </EffectComposer>
 
-        {/* Back Wall */}
-        <Plane
-          args={[10, 6]}
-          position={[0, 3, -5]}
-          receiveShadow
-        >
-          <meshStandardMaterial color="#554444" roughness={1} />
-        </Plane>
+        {/* Room Structure */}
+        <group position={[0, -1.5, 0]}>
+          {/* Floor */}
+          <Plane
+            args={[10, 10]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            receiveShadow
+          >
+            <meshStandardMaterial color="#333" roughness={0.8} />
+          </Plane>
 
-        {/* Left Wall (Window side) */}
-        <Plane
-          args={[10, 6]}
-          position={[-5, 3, 0]}
-          rotation={[0, Math.PI / 2, 0]}
-          receiveShadow
-        >
-          <meshStandardMaterial color="#222" roughness={1} />
-        </Plane>
-      </group>
+          {/* Back Wall */}
+          <Plane
+            args={[10, 6]}
+            position={[0, 3, -5]}
+            receiveShadow
+          >
+            <meshStandardMaterial color="#554444" roughness={1} />
+          </Plane>
 
-      {/* Furniture Placeholders */}
-      <group position={[0, -1.5, 0]}>
-        {/* Sofa */}
-        <Draggable initialPosition={[2, 0.4, 0]}>
-          <Box args={[3, 0.8, 1.2]} castShadow receiveShadow>
-            <meshStandardMaterial color="#885555" roughness={0.2} />
-          </Box>
-        </Draggable>
-
-        {/* Coffee Table */}
-        <Draggable initialPosition={[-0.5, 0.2, 1]}>
-          <Box args={[1.5, 0.4, 1]} castShadow receiveShadow>
-            <meshStandardMaterial color="#222" roughness={0.2} />
-          </Box>
-        </Draggable>
-
-        {/* Chair */}
-        <Draggable initialPosition={[-2.5, 0.4, 0]}>
-          <Box args={[1, 0.8, 1]} rotation={[0, 0.5, 0]} castShadow receiveShadow>
-            <meshStandardMaterial color="#555566" roughness={0.2} />
-          </Box>
-        </Draggable>
-
-        {/* Ottoman */}
-        <Draggable initialPosition={[-2.2, 0.25, 1.5]}>
-          <Box args={[0.8, 0.5, 0.8]} rotation={[0, 0.2, 0]} castShadow receiveShadow>
-            <meshStandardMaterial color="#665555" roughness={0.2} />
-          </Box>
-        </Draggable>
-
-        {/* TV Stand / Cabinet */}
-        <Draggable initialPosition={[0, 0.3, -4]}>
-          <Box args={[6, 0.6, 0.8]} castShadow receiveShadow>
-            <meshStandardMaterial color="#111" roughness={0.2} />
-          </Box>
-        </Draggable>
-
-        {/* Hanging Lamps (Visuals) */}
-        <group position={[0, 4.5, 0]}>
-          <Cylinder args={[0.02, 0.02, 2]} position={[-1.5, 0, 0]}>
-            <meshStandardMaterial color="black" />
-          </Cylinder>
-          <Sphere args={[0.15]} position={[-1.5, -1, 0]}>
-            <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
-          </Sphere>
-
-          <Cylinder args={[0.02, 0.02, 2]} position={[0, 0, 0]}>
-            <meshStandardMaterial color="black" />
-          </Cylinder>
-          <Sphere args={[0.15]} position={[0, -1, 0]}>
-            <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
-          </Sphere>
-
-          <Cylinder args={[0.02, 0.02, 2]} position={[1.5, 0, 0]}>
-            <meshStandardMaterial color="black" />
-          </Cylinder>
-          <Sphere args={[0.15]} position={[1.5, -1, 0]}>
-            <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
-          </Sphere>
+          {/* Left Wall (Window side) */}
+          <Plane
+            args={[10, 6]}
+            position={[-5, 3, 0]}
+            rotation={[0, Math.PI / 2, 0]}
+            receiveShadow
+          >
+            <meshStandardMaterial color="#222" roughness={1} />
+          </Plane>
         </group>
-      </group>
+
+        {/* Furniture Placeholders */}
+        <group position={[0, -1.5, 0]}>
+          {/* Sofa */}
+          <Furniture initialPosition={[2, 0.4, 0]}>
+            <Box args={[3, 0.8, 1.2]} castShadow receiveShadow>
+              <meshStandardMaterial color="#885555" roughness={0.2} />
+            </Box>
+          </Furniture>
+
+          {/* Coffee Table */}
+          <Furniture initialPosition={[-0.5, 0.2, 1]}>
+            <Box args={[1.5, 0.4, 1]} castShadow receiveShadow>
+              <meshStandardMaterial color="#222" roughness={0.2} />
+            </Box>
+          </Furniture>
+
+          {/* Chair */}
+          <Furniture initialPosition={[-2.5, 0.4, 0]}>
+            <Box args={[1, 0.8, 1]} rotation={[0, 0.5, 0]} castShadow receiveShadow>
+              <meshStandardMaterial color="#555566" roughness={0.2} />
+            </Box>
+          </Furniture>
+
+          {/* Ottoman */}
+          <Furniture initialPosition={[-2.2, 0.25, 1.5]}>
+            <Box args={[0.8, 0.5, 0.8]} rotation={[0, 0.2, 0]} castShadow receiveShadow>
+              <meshStandardMaterial color="#665555" roughness={0.2} />
+            </Box>
+          </Furniture>
+
+          {/* TV Stand / Cabinet */}
+          <Furniture initialPosition={[0, 0.3, -4]}>
+            <Box args={[6, 0.6, 0.8]} castShadow receiveShadow>
+              <meshStandardMaterial color="#111" roughness={0.2} />
+            </Box>
+          </Furniture>
+
+          {/* Hanging Lamps (Visuals) */}
+          <group position={[0, 4.5, 0]}>
+            <Cylinder args={[0.02, 0.02, 2]} position={[-1.5, 0, 0]}>
+              <meshStandardMaterial color="black" />
+            </Cylinder>
+            <Sphere args={[0.15]} position={[-1.5, -1, 0]}>
+              <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
+            </Sphere>
+
+            <Cylinder args={[0.02, 0.02, 2]} position={[0, 0, 0]}>
+              <meshStandardMaterial color="black" />
+            </Cylinder>
+            <Sphere args={[0.15]} position={[0, -1, 0]}>
+              <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
+            </Sphere>
+
+            <Cylinder args={[0.02, 0.02, 2]} position={[1.5, 0, 0]}>
+              <meshStandardMaterial color="black" />
+            </Cylinder>
+            <Sphere args={[0.15]} position={[1.5, -1, 0]}>
+              <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
+            </Sphere>
+          </group>
+        </group>
+      </Selection>
     </>
   );
 }
+
