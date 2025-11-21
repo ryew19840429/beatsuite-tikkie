dfdd601f78892db611346460ea51cb0889ac8b83
raymond yong <ryew.1984@gmail.com>
2025-11-21 21:49:43 +0100

    feat: Adjust ambient and main light intensity calculations for improved brightness scaling in the Scene component.

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -4,40 +4,40 @@ import { Box, Plane, Sphere, Cylinder, SoftShadows } from '@react-three/drei';
 
 export function Scene({ brightness }) {
   // Calculate light intensity based on brightness prop (0 to 1)
-  const ambientIntensity = 0.1 + brightness * 0.2;
-  const mainLightIntensity = brightness * 2.5;
+  const ambientIntensity = 0.3 + brightness * 0.5;
+  const mainLightIntensity = brightness * 5;
   const lampColor = '#ffaa77'; // Warm light
 
   return (
     <>
       <SoftShadows size={25} samples={10} focus={0} />
-      
+
       {/* Ambient Light for base visibility */}
       <ambientLight intensity={ambientIntensity} />
 
       {/* Main Ceiling Lights (Simulated as point lights) */}
       {/* Left Lamp */}
-      <pointLight 
-        position={[-1.5, 3, 0]} 
-        intensity={mainLightIntensity} 
-        color={lampColor} 
-        castShadow 
+      <pointLight
+        position={[-1.5, 3, 0]}
+        intensity={mainLightIntensity}
+        color={lampColor}
+        castShadow
         shadow-bias={-0.001}
       />
       {/* Center Lamp */}
-      <pointLight 
-        position={[0, 3, 0]} 
-        intensity={mainLightIntensity} 
-        color={lampColor} 
-        castShadow 
+      <pointLight
+        position={[0, 3, 0]}
+        intensity={mainLightIntensity}
+        color={lampColor}
+        castShadow
         shadow-bias={-0.001}
       />
       {/* Right Lamp */}
-      <pointLight 
-        position={[1.5, 3, 0]} 
-        intensity={mainLightIntensity} 
-        color={lampColor} 
-        castShadow 
+      <pointLight
+        position={[1.5, 3, 0]}
+        intensity={mainLightIntensity}
+        color={lampColor}
+        castShadow
         shadow-bias={-0.001}
       />
 
@@ -54,28 +54,28 @@ export function Scene({ brightness }) {
       {/* Room Structure */}
       <group position={[0, -1.5, 0]}>
         {/* Floor */}
-        <Plane 
-          args={[10, 10]} 
-          rotation={[-Math.PI / 2, 0, 0]} 
+        <Plane
+          args={[10, 10]}
+          rotation={[-Math.PI / 2, 0, 0]}
           receiveShadow
         >
           <meshStandardMaterial color="#333" roughness={0.8} />
         </Plane>
 
         {/* Back Wall */}
-        <Plane 
-          args={[10, 6]} 
-          position={[0, 3, -5]} 
+        <Plane
+          args={[10, 6]}
+          position={[0, 3, -5]}
           receiveShadow
         >
           <meshStandardMaterial color="#554444" roughness={1} />
         </Plane>
 
         {/* Left Wall (Window side) */}
-        <Plane 
-          args={[10, 6]} 
-          position={[-5, 3, 0]} 
-          rotation={[0, Math.PI / 2, 0]} 
+        <Plane
+          args={[10, 6]}
+          position={[-5, 3, 0]}
+          rotation={[0, Math.PI / 2, 0]}
           receiveShadow
         >
           <meshStandardMaterial color="#222" roughness={1} />
@@ -88,7 +88,7 @@ export function Scene({ brightness }) {
         <Box args={[3, 0.8, 1.2]} position={[2, 0.4, 0]} castShadow receiveShadow>
           <meshStandardMaterial color="#885555" />
         </Box>
-        
+
         {/* Coffee Table */}
         <Box args={[1.5, 0.4, 1]} position={[0, 0.2, 1]} castShadow receiveShadow>
           <meshStandardMaterial color="#222" roughness={0.2} />
@@ -98,7 +98,7 @@ export function Scene({ brightness }) {
         <Box args={[1, 0.8, 1]} position={[-2, 0.4, 0]} rotation={[0, 0.5, 0]} castShadow receiveShadow>
           <meshStandardMaterial color="#555566" />
         </Box>
-        
+
         {/* Ottoman */}
         <Box args={[0.8, 0.5, 0.8]} position={[-1.5, 0.25, 1.5]} rotation={[0, 0.2, 0]} castShadow receiveShadow>
           <meshStandardMaterial color="#665555" />
@@ -111,26 +111,26 @@ export function Scene({ brightness }) {
 
         {/* Hanging Lamps (Visuals) */}
         <group position={[0, 4.5, 0]}>
-           <Cylinder args={[0.02, 0.02, 2]} position={[-1.5, 0, 0]}>
-             <meshStandardMaterial color="black" />
-           </Cylinder>
-           <Sphere args={[0.15]} position={[-1.5, -1, 0]}>
-             <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
-           </Sphere>
+          <Cylinder args={[0.02, 0.02, 2]} position={[-1.5, 0, 0]}>
+            <meshStandardMaterial color="black" />
+          </Cylinder>
+          <Sphere args={[0.15]} position={[-1.5, -1, 0]}>
+            <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
+          </Sphere>
 
-           <Cylinder args={[0.02, 0.02, 2]} position={[0, 0, 0]}>
-             <meshStandardMaterial color="black" />
-           </Cylinder>
-           <Sphere args={[0.15]} position={[0, -1, 0]}>
-             <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
-           </Sphere>
+          <Cylinder args={[0.02, 0.02, 2]} position={[0, 0, 0]}>
+            <meshStandardMaterial color="black" />
+          </Cylinder>
+          <Sphere args={[0.15]} position={[0, -1, 0]}>
+            <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
+          </Sphere>
 
-           <Cylinder args={[0.02, 0.02, 2]} position={[1.5, 0, 0]}>
-             <meshStandardMaterial color="black" />
-           </Cylinder>
-           <Sphere args={[0.15]} position={[1.5, -1, 0]}>
-             <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
-           </Sphere>
+          <Cylinder args={[0.02, 0.02, 2]} position={[1.5, 0, 0]}>
+            <meshStandardMaterial color="black" />
+          </Cylinder>
+          <Sphere args={[0.15]} position={[1.5, -1, 0]}>
+            <meshStandardMaterial color="white" emissive={lampColor} emissiveIntensity={brightness} />
+          </Sphere>
         </group>
       </group>
     </>
